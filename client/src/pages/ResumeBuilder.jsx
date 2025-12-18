15bd8614366bfa3a3052617b42432ae181fdd20a
AgniAditya <agniaditya05@gmail.com>
2025-12-18 18:33:41 +0530

    feat: Integrate ResumePreview component into ResumeBuilder for enhanced resume visualization

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom'
 import { dummyResumeData } from "../assets/assets";
 import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Sparkles, User } from 'lucide-react'
 import PersonalInfoForm from "../components/PersonalInfoForm";
+import ResumePreview from "../components/ResumePreview";
 
 const ResumeBuilder = () => {
 
@@ -87,7 +88,13 @@ const ResumeBuilder = () => {
                     </div>
 
                     {/* Right Panel - Preview */}
-                    <div></div>
+                    <div className="lg:col-span-7 max-lg:mt-6">
+                        <div>
+                            {/*--- buttons --- */}
+                        </div>
+                        {/*--- resume preview --- */}
+                        <ResumePreview data={resumedata} template={resumedata.template} accentColor={resumedata.accent_color}/>
+                    </div>
                 </div>
             </div>
         </div>
