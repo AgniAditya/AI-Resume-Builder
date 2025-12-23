efd4cc5b18b8f9163d35fdb2047c0be542afaf54
AgniAditya <agniaditya05@gmail.com>
2025-12-23 14:33:15 +0530

    feat: Add Save Changes button to ResumeBuilder for user interaction

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -112,6 +112,7 @@ const ResumeBuilder = () => {
                                     <SkillForm data={resumedata.skills} onChange={(data) => setresumedata((prev) => ({...prev,skills: data}))}/>
                                 )}
                             </div>
+                            <button className="bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 transition-all rounded-md px-6 py-2 mt-6 text-sm">Save Changes</button>
                         </div>
                     </div>
 
