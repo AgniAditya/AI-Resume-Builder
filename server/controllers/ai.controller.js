9be9d184ce866b112f863e8a548c67ad56912edd
AgniAditya <agniaditya05@gmail.com>
2026-01-03 22:51:24 +0530

    Implements ai features and routes

---
+++ server/controllers/ai.controller.js
@@ -0,0 +1,176 @@
+import { ai } from "../configs/ai.js";
+import { Resume } from "../models/resume.model.js";
+
+//POST: /api/ai/enhance-pro-sum
+const enhanceProfessionalSummary = async (req,res) => {
+    try {
+        const { userContent } = req.body;
+        if(!userContent.trim())
+            return res.status(400).json({message: "Missing required fields."})
+        const aiContent = await ai.models.generateContent({
+            model: process.env.GEMINI_MODEL,
+            contents: userContent,
+            config: {
+                systemInstruction: "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience and career objectives. Make it compelling and ATS-friendly. and only return text no options or anything else."
+            }
+        })
+        const enhanceContent = aiContent.text
+        return res.status(200).json({enhanceContent})
+    } catch (error) {
+        console.log(error.message)
+        return res.status(400).json({message: error.message})
+    }
+}
+
+//POST: /api/ai/enhance-job-desc
+const enhanceJobDescription = async (req,res) => {
+    try {
+        const { userContent } = req.body;
+        if(!userContent.trim())
+            return res.status(400).json({message: "Missing required fields."})
+        const aiContent = await ai.models.generateContent({
+            model: process.env.GEMINI_MODEL,
+            contents: userContent,
+            config: {
+                systemInstruction: "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be 1-2 sentences also highlighting key responsibilities and achievements. Use action verbs and quantifiable results where possible. Make it ATS-friendly. and only return text no options or anything else."
+            }
+        })
+        const enhanceContent = aiContent.text
+        return res.status(200).json({enhanceContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+//POST: /api/ai/upload-resume
+const uploadResume = async (req,res) => {
+    try {
+        const { resumeText,title } = req.body;
+        const userId = req.userId
+        if(!resumeText.trim())
+            return res.status(400).json({message: "Missing required fields."})
+        const aiContent = await ai.models.generateContent({
+            model: process.env.GEMINI_MODEL,
+            contents: `extract data from this resume: ${resumeText}`,
+            config: {
+                systemInstruction: "You are an expert AI agent to extract data from resume.",
+                responseMimeType: 'application/json',
+                responseJsonSchema: {
+                    type: 'object',
+                    properties: {
+                        professional_summary: {
+        type: String,
+        default: ""
+                        },
+                        skills: [
+                            {
+                                type: String
+                            }
+                        ],
+                        personal_info: {
+                            full_name: {
+                                type: String,
+                                default: ''
+                            },
+                            email: {
+                                type: String,
+                                default: ''
+                            },
+                            phone: {
+                                type: String,
+                                default: ''
+                            },
+                            location: {
+                                type: String,
+                                default: ''
+                            },
+                            linkedin: {
+                                type: String,
+                                default: ''
+                            },
+                            website: {
+                                type: String,
+                                default: ''
+                            },
+                            profession: {
+                                type: String,
+                                default: ''
+                            },
+                            image: {
+                                type: String,
+                                default: ''
+                            }
+                        },
+                        experience: [
+                            {
+                                company: {
+                                    type: String,
+                                },
+                                position: {
+                                    type: String,
+                                },
+                                start_date: {
+                                    type: String,
+                                },
+                                end_date: {
+                                    type: String,
+                                },
+                                description: {
+                                    type: String,
+                                },
+                                is_current: {
+                                    type: Boolean,
+                                },
+                            }
+                        ],
+                        project: [
+                            {
+                                name: {
+                                    type: String
+                                },
+                                type: {
+                                    type: String
+                                },
+                                description: {
+                                    type: String
+                                }
+                            }
+                        ],
+                        education: [
+                            {
+                                institution: {
+                                    type: String
+                                },
+                                degree: {
+                                    type: String
+                                },
+                                field: {
+                                    type: String,
+                                },
+                                graduation_date: {
+                                    type: String,
+                                },
+                                gpa: {
+                                    type: String,
+                                },
+                            }
+                        ]
+                    }
+                }
+            }
+        })
+        const extractedData = aiContent.text
+        const parsedData = JSON.parse(extractedData)
+        const newResume = await Resume.create({userId,title,...parsedData})
+
+        return res.status(200).json({resumeId: newResume._id})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+export {
+    enhanceProfessionalSummary,
+    enhanceJobDescription,
+    uploadResume
+}
---
+++ server/routes/ai.route.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { enhanceJobDescription, enhanceProfessionalSummary, uploadResume } from '../controllers/ai.controller.js'
+import { protect } from '../middlewares/auth.middleware.js'
+
+const aiRouter = Router()
+
+aiRouter.post('/enhance-pro-sum',protect,enhanceProfessionalSummary)
+aiRouter.post('/enhance-job-desc',protect,enhanceJobDescription)
+aiRouter.post('/upload-resume',protect,uploadResume)
+
+export default aiRouter
