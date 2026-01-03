90236a862bd1642ebbaf1788cc37ca4ae2861aea
AgniAditya <agniaditya05@gmail.com>
2026-01-03 21:15:01 +0530

    Implements updateResume function to update resume details and also saving the image to imagekit

--- server/controllers/resume.controller.js
+++ server/controllers/resume.controller.js
@@ -1,4 +1,6 @@
-import { Resume } from "../models/resume.model"
+import { Resume } from "../models/resume.model.js"
+import imagekit from "../configs/imagekit.js"
+import fs from 'fs'
 
 //POST: /api/resumes/create
 const createResume = async (req,res) => {
@@ -73,9 +75,40 @@ const getPublicResumeById = async (req,res) => {
     }
 }
 
+//PUT: /api/resumes/update
+const updateResume = async (req,res) => {
+    try {
+        const userId = req.userId
+        const { resumeId, resumeData, removeBackground } = req.body
+        const image = req.file
+        
+        let resumeDataCopy = JSON.parse(resumeData)
+
+        if(image){
+            const imageBufferData = fs.createReadStream(image.path)
+            const response = await imagekit.files.upload({
+                file: imageBufferData,
+                fileName: 'resume.png',
+                folder: 'user-resumes',
+                transformation: {
+                    pre: 'w-300,h-300,fo-face,z-0.75' + (removeBackground ? ',e-bgremove' : '')
+                }
+            });
+            resumeDataCopy.personal_info.image = response.url
+        }
+
+        const updatedResume = await Resume.findByIdAndUpdate({userId,_id: resumeId},resumeDataCopy,{new: true})
+
+        return res.status(200).json({message: "saved successfully",updatedResume})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
 export {
     createResume,
     deleteResume,
     getResumeById,
     getPublicResumeById,
+    updateResume
 }
